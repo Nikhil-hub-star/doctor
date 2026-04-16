8928b3222320021b159eb57b919774cf7edd9f0f
Nikhil <nikhilsharma3612@gmail.com>
2026-04-16 20:15:53 +0530

    Update settings: secure SECRET_KEY and enable DEBUG based on environment variable

--- doctor/settings.py
+++ doctor/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', '0eafcfe79efa9cea84d9dc4fed332003')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
