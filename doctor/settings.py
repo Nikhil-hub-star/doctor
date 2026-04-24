66b150117b025ccfd01d189298082cf644b5e0c9
Nikhil <nikhilsharma3612@gmail.com>
2026-04-24 18:54:47 +0530

    Update settings.py: add CSRF_TRUSTED_ORIGINS for secure requests

--- doctor/settings.py
+++ doctor/settings.py
@@ -28,6 +28,8 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
